831f3f3192e9e6b567afdbb017b96a141ef8ad3a
Aaron Cook <cookx770@umn.edu>
2018-12-28 16:33:57 -0600

    Adding service worker, but it must be served over HTTPS

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-var emtpy_home = '<button type="button" class="btn btn-secondary">\
+var emtpy_home = '<button type="button" class="btn btn-secondary" id="CreateNewSheet">\
   Create new sheet\
-  </button>';
+</button>';
+
+var new_sheet = '<form class="ScoutingSheet">\
+  <div class="form-group">\
+    <label for="teamNumber">Team Number</label>\
+    <input type="number" id="teamNumber" placeholder="Enter team number">\
+  </div>\
+  <div class="form-group">\
+    <label for="teamName">Team Name</label>\
+    <input type="text" id="teamName" placeholder="Enter team name">\
+  </div>\
+  <div class="dropdown">\
+    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="addNewFieldLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
+      Add New...\
+    </a>\
+    <div class="dropdown-menu" aria-labelledby="addNewFieldLink">\
+      <a class="dropdown-item" href="#" id="addNewTrueFalseField">True/False Field</a>\
+      <a class="dropdown-item" href="#" id="addNewNumberField">Number Field</a>\
+      <a class="dropdown-item" href="#" id="addNewTextField">Text Field</a>\
+    </div>\
+  </div>\
+</form>';
 
 var content = document.getElementById('content');
 if (content.innerHTML === '') {
@@ -10,3 +31,14 @@ if (content.innerHTML === '') {
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js');
 }
+
+var newSheetButton = document.getElementById('CreateNewSheet');
+var addTextFieldButton;
+var addTrueFalseFieldButton;
+var addNumberFieldButton;
+newSheetButton.addEventListener('click', function (e) {
+  content.innerHTML = new_sheet;
+  addTextFieldButton = document.getElementById('addNewTextField');
+  addTrueFalseFieldButton = document.getElementById('addNewTrueFalseField');
+  addNumberFieldButton = document.getElementById('addNewNumberField');
+});
